907957785ff5c57654f96c296e0695707411041b
F4bioo <costa.fbo@gmail.com>
2023-01-04 11:35:11 -0300

    Added: [RMG/1-2] Setup gradle buildSrc dependency management

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,6 @@
+object Libs {
+    const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx_version}"
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat_version}"
+    const val material = "com.google.android.material:material:${Versions.material_version}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout_version}"
+}
