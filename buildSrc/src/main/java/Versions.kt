a6bf0ab2dbe84610b851a6bc9d15be8e235503f3
F4bioo <costa.fbo@gmail.com>
2023-01-04 14:15:03 -0300

    Added: [RMG/2-1] Setup Koin

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -5,6 +5,8 @@ object Versions {
     const val material_version = "1.7.0"
     const val constraintLayout_version = "2.1.4"
     const val jacoco_version = "0.8.6"
+    const val koin_version = "2.2.3"
+    const val lifecycle_version = "2.5.1"
 
     // Tests Libs versions
     const val junit_version = "4.13.2"
