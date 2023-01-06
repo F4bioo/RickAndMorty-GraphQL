ab2786f2f4a522935c80af6aa9c1fced544ad8d5
F4bioo <costa.fbo@gmail.com>
2023-01-06 15:12:35 -0300

    Added: [RMG/4-3] Create design module

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -7,4 +7,5 @@ object Modules {
 
     // Libraries
     const val arch = ":libraries:arch"
+    const val design = ":libraries:design"
 }
---
+++ libraries/design/src/main/java/com/fappslab/rickandmortygraphql/design/dsprogress/DsProgressExtension.kt
@@ -0,0 +1,36 @@
+package com.fappslab.rickandmortygraphql.design.dsprogress
+
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.LifecycleOwner
+import com.fappslab.rickandmortygraphql.arch.extension.isNotNull
+
+private const val TAG_DEFAULT = "DsProgress"
+
+// Common
+@Suppress("unused")
+fun LifecycleOwner.dsProgress(
+    block: DsProgress.() -> Unit = {}
+): DsProgress = DsProgress().apply(block)
+
+fun DsProgress.build(
+    shouldShow: Boolean,
+    manager: FragmentManager
+) {
+    manager.hide()
+    if (shouldShow) {
+        show(manager, TAG_DEFAULT)
+    }
+}
+
+// Support
+private fun FragmentManager.hide() {
+    if (isShowing()) {
+        val dialog = findFragmentByTag(TAG_DEFAULT)
+        if (dialog is DsProgress) dialog.dismissAllowingStateLoss()
+    }
+}
+
+private fun FragmentManager.isShowing(): Boolean {
+    executePendingTransactions()
+    return findFragmentByTag(TAG_DEFAULT).isNotNull()
+}
